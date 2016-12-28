5d0dded723eb072da4bfd3bd6865625a31359e37
Oliver Gugenheim <lees@ripco.com>
2016-12-27 18:38:31 -0600

    Refactorings to make code cleaner

--- src/ojplg/skir/ai/AiUtils.java
+++ src/ojplg/skir/ai/AiUtils.java
@@ -11,11 +11,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Stream;
 
-public class AiUtils {
+class AiUtils {
 
-    public static Continent findStrongestUnownedContinent(Player player, Game game){
+    static Continent findStrongestUnownedContinent(Player player, Game game){
         Map<Continent, Float> countryPercentages = computeCountryPercentages(player, game);
         Map<Continent, Float> armyPercentages = computeArmyPercentages(player, game);
 
@@ -34,7 +33,7 @@ public class AiUtils {
         return best;
     }
 
-    public static Map<Continent, Float> computeCountryPercentages(Player player, Game game){
+    private static Map<Continent, Float> computeCountryPercentages(Player player, Game game){
         Map<Continent, Float> ownedPercentages = new HashMap<>();
         for(Continent continent : game.getAllContinents()){
             Float percentage = computeCountryPercentage(player, continent, game);
@@ -43,7 +42,7 @@ public class AiUtils {
         return ownedPercentages;
     }
 
-    public static Map<Continent, Float> computeArmyPercentages(Player player, Game game){
+    private static Map<Continent, Float> computeArmyPercentages(Player player, Game game){
         Map<Continent, Float> ownedPercentages = new HashMap<>();
         for(Continent continent : game.getAllContinents()){
             Float percentage = computeArmyPercentage(player, continent, game);
@@ -52,7 +51,7 @@ public class AiUtils {
         return ownedPercentages;
     }
 
-    public static Float computeCountryPercentage(Player player, Continent continent, Game game){
+    private static Float computeCountryPercentage(Player player, Continent continent, Game game){
         float countryCount = 0;
         float ownedCount = 0;
         for(Country country : continent.getCountries()){
@@ -64,7 +63,7 @@ public class AiUtils {
         return ownedCount/countryCount;
     }
 
-    public static Float computeArmyPercentage(Player player, Continent continent, Game game){
+    private static Float computeArmyPercentage(Player player, Continent continent, Game game){
         float armyCount = 0;
         float myArmyCount = 0;
         for(Country country : continent.getCountries()){
@@ -81,7 +80,7 @@ public class AiUtils {
      * Finds all the possible places where the player has more armies
      * than a neighboring enemy occupied country.
      */
-    public static List<PossibleAttack> findAdvantageousAttacks(Player player, Game game){
+    static List<PossibleAttack> findAdvantageousAttacks(Player player, Game game){
         List<PossibleAttack> advantages = new ArrayList<>();
         for(Country country : game.findCountriesToAttackFrom(player)){
             int myForce = game.getOccupationForce(country);
@@ -95,36 +94,35 @@ public class AiUtils {
         return advantages;
     }
 
-    public static int computeTotalOccupyingForces(List<Country> countries, Game game){
+    static int computeTotalOccupyingForces(List<Country> countries, Game game){
         return countries.stream()
                 .map(game::getOccupationForce)
-                .reduce(new Integer(0), (a,b) -> new Integer(a.intValue() + b.intValue()))
-                .intValue();
+                .reduce(0, (a, b) -> a + b);
     }
 
-    public static int highestOccupyingForce(List<Country> countries, Game game){
+    static int highestOccupyingForce(List<Country> countries, Game game){
         Optional<Integer> possibleHighest = countries.stream()
                 .map(game::getOccupationForce)
-                .max((a,b) -> a.compareTo(b));
+                .max(Integer::compareTo);
         if( possibleHighest.isPresent()){
-            return possibleHighest.get().intValue();
+            return possibleHighest.get();
         }
         return 0;
     }
 
-    public static Country findWeakestPossession(Player player, Game game){
+    static Country findWeakestPossession(Player player, Game game){
         List<Country> possessions = game.findOccupiedCountries(player);
         return possessions.stream()
                 .min((c1, c2) -> (game.getOccupationForce(c1) - game.getOccupationForce(c2)))
                 .get();
     }
 
-    public static int attackingDice(Game game, Country country){
+    static int attackingDice(Game game, Country country){
         return Math.min(Constants.MAXIMUM_ATTACKER_DICE,
                 game.getOccupationForce(country) - 1);
     }
 
-    public static boolean isBorderCountry(Player player, Game game, Country country){
+    static boolean isBorderCountry(Player player, Game game, Country country){
         return game.findBorderCountries(player).contains(country);
     }
 
