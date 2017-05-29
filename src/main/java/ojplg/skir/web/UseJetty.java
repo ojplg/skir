65d0a3de589e3150c4cd39a9b6e98ca26f2c7f77
ojplg <lees@ripco.com>
2017-05-29 18:25:17 -0500

    Fix path to resources for maven style

--- src/main/java/ojplg/skir/web/UseJetty.java
+++ src/main/java/ojplg/skir/web/UseJetty.java
@@ -41,7 +41,7 @@ public class UseJetty  {
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setDirectoriesListed(true);
         resourceHandler.setWelcomeFiles(new String[]{"html/index.html"});
-        resourceHandler.setResourceBase("../out/production/skir/");
+        resourceHandler.setResourceBase("../target/classes/");
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
