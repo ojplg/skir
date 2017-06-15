a23f36af305a73b93b7d0aa8558d1cf5e1b52a2f
ojplg <lees@ripco.com>
2017-06-15 17:28:37 -0500

    Clean up

--- src/main/java/ojplg/skir/state/Player.java
+++ src/main/java/ojplg/skir/state/Player.java
@@ -33,7 +33,6 @@ public class Player {
         this._displayName = displayName;
     }
 
-
     public void updateAttackStatistics(double expectationsDifference, int numberArmyBattles){
         _attackStats.updateStats(expectationsDifference, numberArmyBattles);
     }
