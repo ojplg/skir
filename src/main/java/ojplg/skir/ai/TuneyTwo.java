06b27a017ee7d287317aa194270b5ca08ae71ed1
ojplg <lees@ripco.com>
2017-09-05 13:24:38 -0500

    Manage preset merge properly

--- src/main/java/ojplg/skir/ai/TuneyTwo.java
+++ src/main/java/ojplg/skir/ai/TuneyTwo.java
@@ -127,6 +127,9 @@ public class TuneyTwo implements AutomatedPlayer {
 
     public TuneyTwo(Player player, Map<String, Double> tunings){
         _me = player;
+        for(Map.Entry<String, Double> entry : presetTunings().entrySet()) {
+            tunings.putIfAbsent(entry.getKey(), entry.getValue());
+        }
         _tunings = Collections.unmodifiableMap(tunings);
         _placementMinimums = Arrays.asList(
                 roundedScaledTunedValue(FirstCountryMinimumPlacementKey, 10),
