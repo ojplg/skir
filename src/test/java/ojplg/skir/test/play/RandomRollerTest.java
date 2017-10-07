c5b279b99a64ac947d6e705399ccbd2fc026553a
ojplg <lees@ripco.com>
2017-10-07 10:37:27 -0500

    Try to make probablistic tests never fail

--- src/test/java/ojplg/skir/test/play/RandomRollerTest.java
+++ src/test/java/ojplg/skir/test/play/RandomRollerTest.java
@@ -14,16 +14,16 @@ import static org.junit.Assert.fail;
 public class RandomRollerTest {
 
     @Test
-    public void testOneVOneExpectationsCalculationsWork(){
+    public void testExpectationsCalculations(){
         for(int attacker=1; attacker<=3; attacker++){
             for(int defender=1; defender<=2; defender++){
-                runExpectationsDifferenceTest(1000000, attacker, defender);
+                runExpectationsDifferenceTest(600000, attacker, defender);
             }
         }
     }
 
     private void runExpectationsDifferenceTest(int numberOfTests, int attackingDiceCount, int defendingDiceCount){
-        int allowableAmountOff = numberOfTests / 500;
+        int allowableAmountOff = numberOfTests / 300;
         RandomRoller randomRoller = new RandomRoller(System.currentTimeMillis());
 
         float accumulatedDifferenceFromAttackerExpectations = 0;
@@ -35,8 +35,10 @@ public class RandomRollerTest {
             accumulatedDifferenceFromDefenderExpectations += rolls.defendersExpectationsDifference();
         }
         // These are probablistic tests ... after 10000 rolls, difference from expectations should be (relatively) small
-        assertEquals("Problem with " + attackingDiceCount + " v " + defendingDiceCount, 0, accumulatedDifferenceFromAttackerExpectations, allowableAmountOff);
-        assertEquals("Problem with " + attackingDiceCount + " v " + defendingDiceCount, 0, accumulatedDifferenceFromDefenderExpectations, allowableAmountOff);
+        assertEquals("Problem with " + attackingDiceCount + " v " + defendingDiceCount + " allowable slack " + allowableAmountOff,
+                0, accumulatedDifferenceFromAttackerExpectations, allowableAmountOff);
+        assertEquals("Problem with " + attackingDiceCount + " v " + defendingDiceCount + " allowable slack " + allowableAmountOff,
+                0, accumulatedDifferenceFromDefenderExpectations, allowableAmountOff);
 
         // This test is much more exact ... if the attacker was lucky the defender was unlucky
         assertEquals( accumulatedDifferenceFromAttackerExpectations, -accumulatedDifferenceFromDefenderExpectations, 0.01);
