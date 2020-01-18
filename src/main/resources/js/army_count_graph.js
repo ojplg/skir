e68908e1e642b070d0e86f2e89021269b2a605a2
ojplg <lees@ripco.com>
2020-01-18 11:42:46 -0500

    Smoothie graphs

---
+++ src/main/resources/js/army_count_graph.js
@@ -0,0 +1,49 @@
+function ArmyCountGraph(){
+
+    var blackLine = new TimeSeries();
+    var blueLine = new TimeSeries();
+    var redLine = new TimeSeries();
+    var greenLine = new TimeSeries();
+    var whiteLine = new TimeSeries();
+    var pinkLine = new TimeSeries();
+
+    var counts = {
+        'black': 21,
+        'blue': 21,
+        'red': 21,
+        'green': 21,
+        'white': 21,
+        'pink': 21
+    };
+
+    this.updateCount = function(color, cnt){
+        counts[color] = cnt;
+    }
+
+    setInterval(function() {
+        var x = new Date().getTime();
+        blackLine.append(x, counts['black']);
+        blueLine.append(x, counts['blue']);
+        redLine.append(x, counts['red']);
+        greenLine.append(x, counts['green']);
+        whiteLine.append(x, counts['white']);
+        pinkLine.append(x, counts['pink']);
+
+    }, 1000);
+
+    var smoothie = new SmoothieChart(
+        {
+            grid: { fillStyle:'rgb(60, 60, 60)' }
+            ,millisPerPixel:250
+        });
+
+    smoothie.addTimeSeries(blackLine, { strokeStyle:'rgb(0, 0, 0)' });
+    smoothie.addTimeSeries(blueLine, { strokeStyle:'rgb(0, 0, 255)' });
+    smoothie.addTimeSeries(redLine, { strokeStyle:'rgb(255, 0, 0)' });
+    smoothie.addTimeSeries(greenLine, { strokeStyle:'rgb(0, 255, 0)' });
+    smoothie.addTimeSeries(whiteLine, { strokeStyle:'rgb(255, 255, 255)' });
+    smoothie.addTimeSeries(pinkLine, { strokeStyle:'rgb(250, 175, 190)' });
+
+    smoothie.streamTo(document.getElementById("army-count-graph"), 1000);
+
+}
